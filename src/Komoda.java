e14b8370d7226dc453df9a656ed917ed45641089
Natalia Piszcz <np93@o2.pl>
2018-09-24 22:09:30 +0200

    Dodanie konstruktora, który pozwoli utworzyć obiekt i jednocześnie zainicjalizować jego pola.

--- src/Komoda.java
+++ src/Komoda.java
@@ -3,4 +3,11 @@ public class Komoda {
     String producent;
     int iloscSzuflad;
     double wysokosc;
+
+    Komoda (String k, String p, int iS, double w){
+        kolor = k;
+        producent = p;
+        iloscSzuflad = iS;
+        wysokosc =w;
+    }
 }
--- src/Obiekty.java
+++ src/Obiekty.java
@@ -1,11 +1,8 @@
 public class Obiekty {
     public static void main(String[] args) {
 
-        Komoda komoda1 = new Komoda();
-        komoda1.kolor = "biały";
-        komoda1.producent = "ikea";
-        komoda1.iloscSzuflad = 3;
-        komoda1.wysokosc = 1.2;
+        Komoda komoda1 = new Komoda("biały", "ikea", 3, 1.2);
+
         System.out.println("Twój produkt to " );
         System.out.println("Komoda w kolorze " + komoda1.kolor);
         System.out.println("ze sklepu " + komoda1.producent);
@@ -13,16 +10,12 @@ public class Obiekty {
         System.out.println("Jej wysokość to " + komoda1.wysokosc + "m.");
         System.out.println(" ");
 
-        Komoda komoda2 = new Komoda();
-        komoda2.kolor = "szary";
-        komoda2.producent = "Abra";
-        komoda2.iloscSzuflad = 4;
-        komoda2.wysokosc = 1.5;
+        Komoda komoda2 = new Komoda("szary", "Abra", 4, 1.5);
+
         System.out.println("Twój produkt to " );
         System.out.println("Komoda w kolorze " + komoda2.kolor);
         System.out.println("ze sklepu " + komoda2.producent);
         System.out.println(" i ma " + komoda2.iloscSzuflad + " szuflad.");
         System.out.println("Jej wysokość to " + komoda2.wysokosc + "m.");
-
         }
 }
